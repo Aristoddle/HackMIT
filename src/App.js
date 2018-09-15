469230deb4db7b8d7176dc4b1cd90fbcfed8972d
bkitano <brian.kitano@yale.edu>
2018-09-15 15:10:36 -0400

    app routing

--- src/App.js
+++ src/App.js
@@ -13,14 +13,18 @@ import * as routes from './constants/routes';
 // route components
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Landing from './components/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HashRouter>
-          <Route exact path={routes.SIGN_UP} component={SignUp}/>
-          <Route exact path={routes.LOGIN} component={Login}/>
+          <div>
+            <Route exact path={routes.SIGN_UP} component={SignUp}/>
+            <Route exact path={routes.LOGIN} component={Login}/>
+            <Route exact path={routes.LANDING} component={Landing}/>
+          </div>
         </HashRouter>
       </div>
     );
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,3 +3,5 @@ export const SIGN_UP = '/signup';
 export const LOGIN = '/login';
 export const FORGOT = '/forgot';
 export const VERIFY = '/verify';
+export const HOME = '/home';
+export const LANDING = '/';
